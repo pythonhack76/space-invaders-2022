13040a0a64ddeda5cedf42f6f83c64efb86cca9d
Python The Hack <lrulvoni@gmail.com>
2022-10-09 12:18:18 +0200

    rotation spaceship

--- index.js
+++ index.js
@@ -8,13 +8,9 @@ canvas.height = innerHeight
 
 
 class Player {
-    constructor(){
+    constructor(){ 
 
-      
 
-      
-      
-      
 
         this.velocity = {
             x: 0,
@@ -43,23 +39,92 @@ class Player {
         draw() {
             // c.fillStyle = 'red'
             // c.fillRect(this.position.x, this.position.y, this.width, this.height)
-            if (this.image)
+            //if (this.image)
             c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
+        }
+        update() {
+            if(this.image) {
+            this.draw()
+            this.position.x += this.velocity.x
+            }
+        }
+
+    }
+
+    
+    
+    
+    
+
+    const player = new Player() 
+    const keys = {
+        a:{
+            pressed: false
+        },
+        d: {
+            pressed: false
+        },
+        space: {
+            pressed: false
         }
     }
 
-    const player = new Player() 
-    player.draw()
+    //player.draw()
 
     function animate() {
         requestAnimationFrame(animate)
-
         c.fillStyle = 'black'
         c.fillRect(0,0, canvas.width, canvas.height)
-        player.draw()
+        player.update()
+
+        if (keys.a.pressed && player.position.x >= 0){
+            player.velocity.x = -5
+        } else if (keys.d.pressed) {
+            player.velocity.x = 5
+        } else {
+            player.velocity.x = 0
+        }
     }
 
 
     animate() 
+
+    addEventListener('keydown', ({key}) => {
+        switch (key) {
+            case 'a':
+            console.log('left')
+            
+            keys.a.pressed = true
+            break
+            case 'd':
+                console.log('right')
+                
+                keys.d.pressed = true
+                break
+                case ' ':
+                    console.log('space')
+                    break
+        }
+    })
+
+
+    addEventListener('keyup', ({key}) => {
+        switch (key) {
+            case 'a':
+            console.log('left')
+            
+            keys.a.pressed = false
+            break
+            case 'd':
+                console.log('right')
+               
+                keys.d.pressed = false
+                break
+                case ' ':
+                    console.log('space')
+                    break
+        }
+    })
+
 
 
