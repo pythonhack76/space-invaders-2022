84c8e870c8be2e59d0833b592a4f75e9dd130d13
Python The Hack <lrulvoni@gmail.com>
2022-10-09 12:02:24 +0200

    update spaceship movement

--- index.js
+++ index.js
@@ -10,10 +10,11 @@ canvas.height = innerHeight
 class Player {
     constructor(){
 
-        this.position = {
-            x: 200,
-            y: 200
-        }
+      
+
+      
+      
+      
 
         this.velocity = {
             x: 0,
@@ -27,6 +28,11 @@ class Player {
             this.image = image
             this.width = image.width * scale
             this.height = image.height  * scale
+
+            this.position = {
+                x: canvas.width / 2 - this.width / 2,
+                y: canvas.height - this.height - 20
+            }
 
          }
 
