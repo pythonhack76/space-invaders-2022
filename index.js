d77522519e372b77b948278d2258aa80f476c43d
Python The Hack <lrulvoni@gmail.com>
2022-10-09 11:14:57 +0200

    creation player

--- index.js
+++ index.js
@@ -20,17 +20,40 @@ class Player {
             y: 0
          }
 
-         //this.image = 
-         this.width = 100
-         this.height = 100 
+         const image = new Image()
+         image.src = './img/spaceship.png'
+         image.onload = () => {
+            const scale = 0.15
+            this.image = image
+            this.width = image.width * scale
+            this.height = image.height  * scale
+
+         }
+
+        
+        
+        
         }
         draw() {
-            c.fillStyle = 'red'
-            c.fillRect(this.position.x, this.position.y, this.width, this.height)
+            // c.fillStyle = 'red'
+            // c.fillRect(this.position.x, this.position.y, this.width, this.height)
+            if (this.image)
+            c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
         }
     }
 
     const player = new Player() 
     player.draw()
+
+    function animate() {
+        requestAnimationFrame(animate)
+
+        c.fillStyle = 'black'
+        c.fillRect(0,0, canvas.width, canvas.height)
+        player.draw()
+    }
+
+
+    animate() 
 
 
